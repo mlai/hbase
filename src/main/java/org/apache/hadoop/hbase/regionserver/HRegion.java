19a1fda54aed287098ba3e0695c6f6f434f74d51
Mingjie Lai <mingjie_lai@trendmicro.com>
2010-08-26 23:12:09 -0700

    Fixed test failure: TestHRegion, TestHeapSize, TestRowResource

--- src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
+++ src/main/java/org/apache/hadoop/hbase/regionserver/HRegion.java
@@ -1569,9 +1569,6 @@ public class HRegion implements HeapSize { // , Writable{
         result = get(get);
         if (coprocessorHost != null) {
           result = coprocessorHost.onGet(get, result);
-          if (result.isEmpty()) {
-            return false;
-          }
         }
         boolean matches = false;
         if (result.size() == 0 && expectedValue.length == 0) {
@@ -3070,7 +3067,7 @@ public class HRegion implements HeapSize { // , Writable{
 
   public static final long FIXED_OVERHEAD = ClassSize.align(
       (4 * Bytes.SIZEOF_LONG) + Bytes.SIZEOF_BOOLEAN +
-      (22 * ClassSize.REFERENCE) + ClassSize.OBJECT + Bytes.SIZEOF_INT);
+      (20 * ClassSize.REFERENCE) + ClassSize.OBJECT + Bytes.SIZEOF_INT);
 
   public static final long DEEP_OVERHEAD = ClassSize.align(FIXED_OVERHEAD +
       ClassSize.OBJECT + (2 * ClassSize.ATOMIC_BOOLEAN) +
